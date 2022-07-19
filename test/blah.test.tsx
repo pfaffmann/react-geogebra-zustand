91dc1e333a95a1c4e73cae3705e2b2e6295294c5
pfaffic <48207288+pfaffmann@users.noreply.github.com>
2022-07-19 15:56:39 +0200

    Update blah.test.tsx

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { GeoGebra } from '../src';
+import { GeoGebra, GeoGebraScriptInjector } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<GeoGebra id="app" />, div);
+    ReactDOM.render(
+      <>
+        <GeoGebraScriptInjector />
+        <GeoGebra id="app" />
+      </>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
