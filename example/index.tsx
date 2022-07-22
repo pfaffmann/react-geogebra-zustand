235f28394fae0789d239e113a9503b778a53cd7c
pfaffic <48207288+pfaffmann@users.noreply.github.com>
2022-07-22 23:27:53 +0200

    example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'react-app-polyfill/ie11';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { App } from './src/App';
 
 const container = document.getElementById('root');
